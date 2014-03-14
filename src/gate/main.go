582cc9218041fbd16aece7784a6294a84be36a93
zhoufan <zhoufan@nibirutech.com>
2014-03-14 12:37:05 +0800

    test build

--- src/gate/main.go
+++ src/gate/main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/zfbp/go_juice/src/gogame"
+	"net"
+	"reflect"
 )
 
+func procductor(writer *net.Conn) {
+	fmt.Println(reflect.TypeOf(writer).String())
+
+	// for {
+	// 	b := make([]byte, 1000)
+	// 	writer.Write(b)
+	// }
+}
+
+func customer(reader *net.Conn) {
+	fmt.Println(reflect.TypeOf(reader).String())
+	// for {
+	// 	bytes := make([]byte, 2048)
+	// 	reader.Read(bytes)
+	// }
+
+}
+
 func main() {
-	o := gogame.NewObject("asdfasdfsdf")
-	fmt.Println(o.Name())
+	conn, error := net.Dial("tcp", "google.com:https")
+	fmt.Println(conn.RemoteAddr(), error)
+
+	writer, reader := net.Pipe()
+	fmt.Println(reflect.TypeOf(writer).String(), reflect.TypeOf(reader).String())
+
+	go procductor(&writer)
+	go customer(&reader)
+
+	//select {}
 }
