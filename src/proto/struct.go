825bb829c584e38a6707e3f8ff1c235760af1d9f
yangzhengyi <yangzhengyi@gmail.com>
2014-04-04 15:41:47 +0800

    add config

--- src/proto/struct.go
+++ src/proto/struct.go
@@ -2,6 +2,7 @@ package proto
 
 // name 全部大写
 //结构体自动首字母大写
+
 type Teq struct {
 	X int
 	Y int
@@ -11,6 +12,22 @@ desc:XXXXX
 
 name:XX2
 desc:XXXXXXX
+*/
+
+type CfgFlush struct {
+	Db    string
+	Url   string
+	Modem string
+} /*
+name:CFG_FLUSH_REQ
+desc:flush config
+*/
+
+type CfgRsp struct {
+	State bool
+} /*
+name:CFG_FLUSH_RSP
+desc: config respond
 */
 
 type OtherUse struct {
