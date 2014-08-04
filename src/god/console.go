e003e3cd7bf0cd26b3943562ba18bb4ec198a501
Fan Zhou <zzffbbpp@gmail.com>
2014-08-04 13:26:11 +0800

    console input

--- src/god/console.go
+++ src/god/console.go
@@ -1,14 +1,35 @@
 package god
 
+/*
+#include <readline/readline.h>
+#include <stdlib.h>
+#cgo LDFLAGS: -lreadline
+*/
+import "C"
+
 import (
-	"runtime"
+	"fmt"
+	"unsafe"
 )
 
 type Console struct {
 }
 
 func (c *Console) Run() {
-	for {
-		runtime.Gosched()
+	prompt := C.CString("god> ")
+	defer C.free(unsafe.Pointer(prompt))
+	var line string
+
+	for line != "q" {
+		cline := C.readline(prompt)
+		defer C.free(unsafe.Pointer(cline))
+		if cline == nil {
+			fmt.Printf("\n")
+			break
+		}
+
+		C.add_history(cline)
+		line = C.GoString(cline)
+		fmt.Printf("%v\n", line)
 	}
 }
