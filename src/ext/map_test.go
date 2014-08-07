66bda0e861b5eee5c4cc941e0f5bb0fbf4cbd54a
Fan Zhou <zzffbbpp@gmail.com>
2014-08-07 18:06:07 +0800

    init before benchmark

--- src/ext/map_test.go
+++ src/ext/map_test.go
@@ -1,6 +1,7 @@
 package ext
 
 import (
+	"math"
 	"runtime"
 	"sync"
 	"testing"
@@ -13,12 +14,14 @@ var (
 type intBoolMap map[int]bool
 type uint64BoolMap map[uint64]bool
 
-func TestMapParallel(t *testing.T) {
-
-}
-
 func BenchmarkMapSet(b *testing.B) {
 	m := make(intBoolMap)
+
+	for i := 0; i < 100000; i++ {
+		m[i] = true
+	}
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m[i] = true
 	}
@@ -26,21 +29,42 @@ func BenchmarkMapSet(b *testing.B) {
 
 func BenchmarkMapGet(b *testing.B) {
 	m := make(intBoolMap)
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < int(math.Max(float64(b.N), 100000)); i++ {
 		m[i] = true
 	}
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Assert(m[i])
 	}
 }
 
+func initParallelMap(m ParallelMap) {
+	for i := 0; i < 100000; i++ {
+		m.Set(i, i)
+	}
+}
+
 func testParallelMap(t *testing.T, m ParallelMap) {
-	AssertT(t, m.Set(10, 100))
-	v, ok := m.Get(10).(int)
-	AssertT(t, ok && v == 100)
-	AssertT(t, m.Delete(10))
-	AssertT(t, m.Get(10) == nil)
+	initParallelMap(m)
+
+	test := func(k, v interface{}) {
+		AssertT(t, m.Set(k, v))
+		ret, ok := m.Get(k).(uint64)
+		AssertT(t, ok && v == ret)
+		AssertT(t, m.Delete(k))
+		AssertT(t, m.Get(k) == nil)
+	}
+
+	for i := -10000; i < 10000; i++ {
+		v := RandomUint64()
+		test(i, v)
+	}
+
+	for i := 0; i < 10000; i++ {
+		k, v := string(RandomUint64()), RandomUint64()
+		test(k, v)
+	}
 }
 
 func TestChanMap(t *testing.T) {
@@ -50,7 +74,9 @@ func TestChanMap(t *testing.T) {
 func benchmarkParallelMap(b *testing.B, m ParallelMap) {
 	var wg sync.WaitGroup
 	wg.Add(3)
+	initParallelMap(m)
 
+	b.ResetTimer()
 	go func() {
 		defer wg.Done()
 		for i := 0; i < b.N; i++ {
@@ -81,6 +107,9 @@ func benchmarkParallelMap(b *testing.B, m ParallelMap) {
 }
 
 func benchmarkMapSet(b *testing.B, m ParallelMap) {
+	initParallelMap(m)
+
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			AssertB(b, m.Set(RandomUint64(), true))
@@ -89,6 +118,9 @@ func benchmarkMapSet(b *testing.B, m ParallelMap) {
 }
 
 func benchmarkMapGet(b *testing.B, m ParallelMap) {
+	initParallelMap(m)
+
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			m.Get(RandomUint64())
@@ -97,6 +129,9 @@ func benchmarkMapGet(b *testing.B, m ParallelMap) {
 }
 
 func benchmarkMapDelete(b *testing.B, m ParallelMap) {
+	initParallelMap(m)
+
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			AssertB(b, m.Delete(RandomUint64()))
@@ -108,16 +143,13 @@ func BenchmarkChanMap(b *testing.B) {
 	benchmarkParallelMap(b, NewChanMap())
 }
 func BenchmarkChanMapSet(b *testing.B) {
-	m := NewChanMap()
-	benchmarkMapSet(b, m)
+	benchmarkMapSet(b, NewChanMap())
 }
 
 func BenchmarkChanMapGet(b *testing.B) {
-	m := NewChanMap()
-	benchmarkMapGet(b, m)
+	benchmarkMapGet(b, NewChanMap())
 }
 
 func BenchmarkChanMapDelete(b *testing.B) {
-	m := NewChanMap()
-	benchmarkMapDelete(b, m)
+	benchmarkMapDelete(b, NewChanMap())
 }
